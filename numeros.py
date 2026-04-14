994eceef218c0f8ded5ef27ceb55380a952afd5a
Diego-434 <di.celisr@duocuc.cl>
2026-04-14 11:44:04 -0400

    primer commit

---
+++ numeros.py
@@ -0,0 +1,6 @@
+# nom=input("Ingrese su nombre: ")
+# n1=int(input("Ingrese un numero: "))
+# n2=int(input("Ingrese otro numero: "))
+# print("Hola", nom, "La suma de los 2 numeros es:", n1+n2, "y la resta es:", n1-n2)
+
+
---
+++ test.py
@@ -0,0 +1,4 @@
+nombre=input("Ingrese su nombre: ")
+edad=int(input("Ingrese su edad: "))
+
+print("Hola", nombre, "su edad en 5 años será:", edad+5)
